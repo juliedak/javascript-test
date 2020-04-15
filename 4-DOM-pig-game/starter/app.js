2018b111aad7259926511c79c3321e8da1cdf055
juliedakouré <julie.dakoure@outlook.fr>
2020-04-15 14:39:21 +0200

    lectre 50 done

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -32,10 +32,33 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   diceDOM.style.display = 'block';
   diceDOM.src = 'dice-' + dice + '.png';
   //3. Update the round score IF he rolled number was NOT a 1
+  if (dice !== 1) {
+      //Add score
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+      // Next player
+      activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+      roundScore = 0;
+
+      document.getElementById('current-0').textContent = '0';
+      document.getElementById('current-1').textContent = '0';
+
+      document.querySelector('.player-0-panel').classList.toggle('active');
+      document.querySelector('.player-1-panel').classList.toggle('active');
+
+      //document.querySelector('.player-0-panel').classList.remove('active');
+      //document.querySelector('.player-1-panel').classList.add('active');
+      document.querySelector('.dice').style.display = 'none';
+  }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  //
 
-// document.querySelector('#current-' + activePlayer).textContent = dice;
+});
+
+//  = dice;
 // //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 // var x = document.querySelector('#score-0').textContent;
